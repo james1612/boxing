433d86bd6d14b8edb3d794f80195c6dc90dd6115
james1612 <james.irish18@gmail.comgit add *.javagit config --global user.email james.irish18@gmail.com>
2019-09-09 18:37:53 +0100

    made functions that query api

--- boxerScripts.js
+++ boxerScripts.js
@@ -1,3 +1,70 @@
+// const data = JSON.stringify({
+// 	"id": 1,
+// 	"firstName": "Caneloooooooooooooooooblahahhahaa",
+// 	"lastName": "Alvarez",
+// 	"age": 235,
+// 	"nationality": "Mexico"
+// });
+
+
+// function formToObject(form) {
+//     const formDataObj = {};
+//     for (let element of form.elements) {
+//         if (element.id) {
+// 			formDataObj[element.id] = element.value;
+//         }
+// 	}
+//     return formDataObj;
+// }
+
+function formToObject(form) {
+    const formDataObj = {};
+    for (let element of form.elements) {
+        if (element.id) {
+			formDataObj[element.id] = element.value;
+        }
+	}
+    return formDataObj;
+}
+
+
+function postBoxer(form) {
+	data = formToObject(form);
+	console.log(data);
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.onload = () => {
+			if (xhr.status == 200) {
+				resolve(xhr.response);
+			}
+			else {
+				reject('Request failed!');
+			}
+		}
+		xhr.open('POST', 'http://localhost:9000/boxers');
+		xhr.setRequestHeader("content-type", "application/json");
+		xhr.send(data);
+	});
+}
+
+// function postBoxer() {
+// 	return new Promise((resolve, reject) => {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.onload = () => {
+// 			if (xhr.status == 200) {
+// 				resolve(xhr.response);
+// 			}
+// 			else {
+// 				reject('Request failed!');
+// 			}
+// 		}
+// 		xhr.open('POST', 'http://localhost:9000/boxers');
+// 		xhr.setRequestHeader("content-type", "application/json");
+// 		xhr.send(data);
+// 	});
+// }
+
+
 
 
 function addBoxer() {
